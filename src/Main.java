3ceed466da88db4bcf3e96e89a683c9b947d444f
Lucas <goulart2041@gmail.com>
2025-09-05 21:09:50 -0300

    :feat: desenvolvimento calculadora IMC

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,38 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        gerarTabuada();
+        /*gerarTabuada();*/
+        calcularIMC();
     }
 
-    public static void gerarTabuada(){
+    public static void gerarTabuada() {
         System.out.println("Informe a tabuada que você quer gerar: ");
         int numeroTabuada = scanner.nextInt();
-        for(int i = 1; i < 10; i++){
+        for (int i = 1; i < 10; i++) {
             int resultado = i * numeroTabuada;
             System.out.printf("%d * %d = %d\n", i, numeroTabuada, resultado);
         }
+    }
+
+    public static void calcularIMC() {
+        System.out.println("Informe o seu peso: ");
+        double peso = scanner.nextDouble();
+
+        System.out.println("Inforome a sua altura: ");
+        double altura = scanner.nextDouble();
+
+        scanner.close();
+
+        final var _IMC = peso / (altura * altura);
+
+        System.out.printf("Seu IMC é: %.2f%n\n", _IMC);
+
+        if (_IMC <= 18.5) System.out.println("Abaixo do peso ideal");
+        else if (_IMC > 18.5 && _IMC <= 24.9) System.out.println("Peso ideal");
+        else if (_IMC > 24.9 && _IMC <= 29.9) System.out.println("Levemente acima do peso");
+        else if (_IMC > 29.9 && _IMC <= 34.9) System.out.println("Obesidade Grau I");
+        else if (_IMC > 34.9 && _IMC <= 39.9) System.out.println("Obesidade Grau II (Severa");
+        else System.out.println("Obesidade III (Mórbida");
+
     }
 }
